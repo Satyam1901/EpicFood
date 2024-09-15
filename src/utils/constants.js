97a526966b1f4e66c3decdda9df74bb8c55112ba
Satyam1901 <satyamsingh1901@gmail.com>
2024-09-16 00:04:32 +0530

    gitignore Updation

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const MENU_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-
-export const MENU_API = "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId="
-
-export const MENU_List = "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65420&lng=77.23730&restaurantId=831065&submitAction=ENTER"
-
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/"
